6384104cbf0e653fc53632ea1ec270940a5ba99c
Koaln <1124013806@qq.com>
2021-02-28 22:37:43 +0800

    Initial commit

---
+++ src/main/java/com/lxl/pbpserver/PbpserverApplication.java
@@ -0,0 +1,13 @@
+package com.lxl.pbpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbpserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbpserverApplication.class, args);
+    }
+
+}
